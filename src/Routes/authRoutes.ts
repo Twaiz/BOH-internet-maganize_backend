12d512a0ca3a1eb7629add03e47b59d499339539
Bohdan Khomenko <homenkobodya@gmail.com>
2024-12-20 21:38:13 +0200

    add[authRoutes - forgotPassword]: added new route forgotPassword

--- src/Routes/authRoutes.ts
+++ src/Routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import { logIn, signUp } from '../Controllers';
+import { logIn, signUp, forgotPassword } from '../Controllers';
 
 const route = express.Router();
 
 route.post('/signup', signUp);
 route.post('/login', logIn);
+
+route.post('/forgotPassword', forgotPassword);
 
 export { route as authRoute };
